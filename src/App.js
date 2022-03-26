0a02c0cf1c24e28abd4e69384ca09a7eed93576e
unknown <hossen11@cse.pstu.ac.bd>
2022-03-26 23:14:34 +0100

    inial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./assets/App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import TodoList from './components/todo/TodoList';
+
+const App = () => {
+  
+  return (
+    <div className="app">
+      <TodoList />
+    </div>
+  )
+}
+
+export default App;
